8bf220ea9c585ac8441bbb71b88b8130722353b0
Minsoo <zlemzlem5656@naver.com>
2020-05-25 01:24:07 +0900

    5강 Props 활용

---
+++ new-cli-project/src/components/UserDetail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h3>자세한 회원 정보를 확인합니다.</h3>
+    <p>{{ prop_name }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserDetail",
+  data() {
+    return {};
+  },
+  props: ["prop_name"]
+};
+</script>
--- new-cli-project/src/main.js
+++ new-cli-project/src/main.js
@@ -1,14 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import Status from "./Status";
 
-Vue.config.productionTip = false
+// 전역으로 선언
+Vue.component("AppStatus", Status);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
