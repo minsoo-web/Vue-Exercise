0c84c61580d2002fea141b5b641358179a047552
Minsoo <zlemzlem5656@naver.com>
2020-05-29 16:42:02 +0900

    환경 설정 및 vuetify 플러그인 추가

--- vuex-router/README.md
+++ vuex-router/README.md
@@ -5,4 +5,7 @@
 맨땅에 개발하기님 유튜브 재생목록  
 [Vuex & Router](https://www.youtube.com/watch?v=TSyS3hMaDfA&list=PLZzSdj89sCN2BNq2ugCT634Cazda_Ytl-)
 
-## 01. 
+## 01 강
+
+Vuex 와 Router를 이용해 로그인을 하고 상태관리를 하는 프로젝트입니다
+
---
+++ vuex-router/src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-container style="max-width: 450px" fill-height>
+    <v-layout align-center>
+      <v-flex xs12>
+        <v-card>
+          <v-toolbar flat>
+            <v-toolbar-title>Login</v-toolbar-title>
+          </v-toolbar>
+          <div class="pa-3">
+            <v-text-field v-model="email" label="email"> </v-text-field>
+            <v-text-field v-model="password" label="password" type="password">
+            </v-text-field>
+          </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+};
+</script>
+
+<style></style>
