5f872fcbdfef551e756d666c2ef43d5a6fe08941
Minsoo <zlemzlem5656@naver.com>
2020-05-31 00:47:20 +0900

    axios 6 강
    <description>
    헤더에 토근 담아 요청보내기
    axios 를 중복해서 사용하는 것에 대해 실습 했습니다.

--- vuex-router/src/store/index.js
+++ vuex-router/src/store/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import router from "@/router/index.js";
+import axios from "axios";
 
 Vue.use(Vuex);
 
@@ -47,20 +48,53 @@ export default new Vuex.Store({
   actions: {
     // 비지니스 로직
     // 로그인을 시도
-    login({ state, commit }, singninObj) {
-      let selectedUser = null;
-      state.allUsers.forEach((user) => {
-        user.email == singninObj.email ? (selectedUser = user) : null;
-      });
-      if (
-        selectedUser === null ||
-        selectedUser.password !== singninObj.password
-      ) {
-        commit("loginError", selectedUser);
-      } else {
-        commit("loginSuccess", selectedUser);
-        router.push({ name: "Mypage" });
-      }
+    login({ commit }, singninObj) {
+      axios
+        .post("https://reqres.in/api/login", {
+          email: singninObj.email,
+          password: singninObj.password,
+        })
+        .then((res) => {
+          let token = res.data.token;
+          let config = {
+            headers: {
+              "access-toketn": token,
+            },
+          };
+          axios
+            .get("https://reqres.in/api/users/2", config)
+            .then((response) => {
+              let obj = {
+                avatar: response.data.data.avatar,
+                email: response.data.data.email,
+                first_name: response.data.data.first_name,
+                id: response.data.data.id,
+                last_name: response.data.data.last_name,
+              };
+              commit("loginSuccess", obj);
+            })
+            .catch((err) => {
+              console.log(err);
+              alert("로그인 실패");
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("로그인 실패");
+        });
+      // let selectedUser = null;
+      // state.allUsers.forEach((user) => {
+      //   user.email == singninObj.email ? (selectedUser = user) : null;
+      // });
+      // if (
+      //   selectedUser === null ||
+      //   selectedUser.password !== singninObj.password
+      // ) {
+      //   commit("loginError", selectedUser);
+      // } else {
+      //   commit("loginSuccess", selectedUser);
+      //   router.push({ name: "Mypage" });
+      // }
     },
     logout({ commit }) {
       commit("logout");
--- vuex-router/src/views/Login.vue
+++ vuex-router/src/views/Login.vue
@@ -2,20 +2,15 @@
   <v-container style="max-width: 450px" fill-height>
     <v-layout align-center>
       <v-flex xs12>
-        <v-alert type="success" :value="isLogin">
-          I'm a success alert.
-        </v-alert>
-        <v-alert type="error" :value="isLoginError">
-          아이디와 비밀번호를 확인해주세요
-        </v-alert>
+        <v-alert type="success" :value="isLogin">I'm a success alert.</v-alert>
+        <v-alert type="error" :value="isLoginError">아이디와 비밀번호를 확인해주세요</v-alert>
         <v-card>
           <v-toolbar flat>
             <v-toolbar-title>Login</v-toolbar-title>
           </v-toolbar>
           <div class="pa-3">
-            <v-text-field v-model="email" label="email"> </v-text-field>
-            <v-text-field v-model="password" label="password" type="password">
-            </v-text-field>
+            <v-text-field v-model="email" label="email"></v-text-field>
+            <v-text-field v-model="password" label="password" type="password"></v-text-field>
             <v-btn
               block
               large
@@ -27,8 +22,7 @@
                   password: password,
                 })
               "
-              >로그인</v-btn
-            >
+            >로그인</v-btn>
           </div>
         </v-card>
       </v-flex>
@@ -38,20 +32,21 @@
 
 <script>
 import { mapState, mapActions } from "vuex";
+
 export default {
   name: "Login",
   data() {
     return {
       email: "",
-      password: "",
+      password: ""
     };
   },
   computed: {
-    ...mapState(["isLogin", "isLoginError"]),
+    ...mapState(["isLogin", "isLoginError"])
   },
   methods: {
-    ...mapActions(["login"]),
-  },
+    ...mapActions(["login"])
+  }
 };
 </script>
 
