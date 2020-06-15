f114632767f20896840ebeb2ce9704ccb2aa1cff
Minsoo <zlemzlem5656@naver.com>
2020-06-15 20:56:42 +0900

    docs : 라우터 및 UI 설정
    <description>
    bootstrap vue 를 사용한 ui 생성
    라우팅 설정

---
+++ mevn/front/src/views/Login/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="mt-4">
+    <b-form @submit="onSubmit">
+      <b-form-group
+        id="input-group-1"
+        label="Email address:"
+        label-for="input-1"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-2"
+        label="Your Password:"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
+          type="password"
+          v-model="form.password"
+          required
+          placeholder="Enter Password"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Login</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ mevn/front/src/views/SignUp/index.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="mt-4">
+    <b-form @submit="onSubmit">
+      <b-form-group id="input-group-1" label="Name:" label-for="input-1">
+        <b-form-input
+          id="input-1"
+          v-model="form.name"
+          type="text"
+          required
+          placeholder="Enter Name"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-2"
+        label="Email address:"
+        label-for="input-2"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="form.email"
+          type="email"
+          required
+          placeholder="Enter email"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-3"
+        label="Your Password:"
+        label-for="input-3"
+      >
+        <b-form-input
+          id="input-3"
+          type="password"
+          v-model="form.password"
+          required
+          placeholder="Enter Password"
+        ></b-form-input>
+      </b-form-group>
+      <b-form-group
+        id="input-group-4"
+        label="Re Enter Your Password:"
+        label-for="input-4"
+      >
+        <b-form-input
+          id="input-4"
+          type="password"
+          v-model="form.re_password"
+          required
+          placeholder="Enter Password Again"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">SignUp</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+        password: "",
+        re_password: "",
+      },
+    };
+  },
+  methods: {
+    onSubmit(evt) {
+      evt.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+  },
+};
+</script>
+
+<style></style>
