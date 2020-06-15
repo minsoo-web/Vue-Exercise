37dd6c6514e9d4b3b2ca7761495a8ec3fb616e27
Minsoo <zlemzlem5656@naver.com>
2020-06-15 20:54:21 +0900

    Update : import 에러 해결 & validation
    <description>
    const / require  로 변경
    express-validatior 패키지 사용으로
    데이터 검증

--- mevn/back/app.js
+++ mevn/back/app.js
@@ -1,10 +1,57 @@
-import express from "express";
-import monogoose from "mongoose";
-import dotenv from "dotenv";
+const express = require("express");
+const monogoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+
+const { body, validationResult } = require("express-validator");
 
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+
+app.post(
+  "/auth/signup",
+  [
+    body("email").isEmail().withMessage("please type valid email"),
+    body("password")
+      .trim()
+      .isLength({ min: 6 })
+      .withMessage("Password must be greater than 6 character"),
+    body("name").trim().not().isEmpty().withMessage("Name field is required"),
+  ],
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation failed");
+      error.statusCode = 422;
+      error.data = errors.array();
+
+      throw error;
+    }
+
+    console.log(req.body);
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+    res.json({
+      message: "success",
+    });
+  }
+);
+
+app.use((error, req, res, next) => {
+  const statusCode = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(statusCode).json({
+    message,
+    data,
+  });
+});
 
 monogoose
   .connect(
