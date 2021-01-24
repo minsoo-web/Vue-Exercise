a5cb4558d1694cd3c5237055b724f828da98ea99
minsoo-web <zlemzlem5656@naver.com>
2021-01-24 22:03:04 +0900

    [추가] vue-calendar 연습

---
+++ vue-calendar/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
---
+++ vue-calendar/src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <vc-date-picker is-range v-model="range" />
+    <p>
+      {{ range.start }}
+    </p>
+    <p>
+      {{ range.end }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      range: {
+        start: Date.now(),
+        end: new Date()
+      }
+    };
+  }
+};
+</script>
+
+<style></style>
